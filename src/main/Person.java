bda524defcc355d166f00cb4cd9f91c985e67fe4
Rohan <rohantinna3@gmail.com>
2021-10-14 14:53:59 -0400

    Created Controller, Person, and User classes. Added basic functionality to Controller class.
    Added signUp() method and main() method in Controller class.
    
    Signed-off-by: Rohan <rohan@rohantinna.com>

---
+++ src/main/Person.java
@@ -0,0 +1,13 @@
+public abstract class Person {
+    public String name;
+    private String email;
+    private String phone;
+    private String groups;
+
+    public Person(String name, String email) {
+
+    }
+
+
+
+}
