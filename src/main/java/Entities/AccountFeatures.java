3aa2278173ace6b3e353e8804d173af84dc39207
Wasee Alam <71355734+WaseeA@users.noreply.github.com>
2021-10-16 23:53:25 -0400

    Delete src directory

--- src/main/java/Entities/AccountFeatures.java
+++
@@ -1,9 +0,0 @@
-package Entities;
-
-public interface AccountFeatures {
-    /**
-     * Generate a unique integer.
-     * @return an integer representing the UID (unique integer).
-     */
-    int generateUID();
-}
