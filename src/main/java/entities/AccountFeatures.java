b954699a7ea8fe6bc8ee830b04147204ae2a865e
Rohan <rohan@rohantinna.com>
2021-11-07 15:37:47 -0500

    Cleaning up code for Spring integration

--- src/main/java/entities/AccountFeatures.java
+++
@@ -1,9 +0,0 @@
-package entities;
-
-public interface AccountFeatures {
-    /**
-     * Generate a unique integer.
-     * @return an integer representing the UID (unique integer).
-     */
-    String generateUUID();
-}
