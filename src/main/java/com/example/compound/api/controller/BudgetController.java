774a2e82d9197fcd70baf656c86ffebef6241a58
Lingyun Jiang <rac.angel.jly@gmail.com>
2021-12-09 01:06:20 -0500

    Deleted unused files and moved springBudgetController to api.controller package.

--- src/main/java/com/example/compound/api/controller/BudgetController.java
+++ src/main/java/com/example/compound/api/controller/BudgetController.java
@@ -1,184 +1,166 @@
 package com.example.compound.api.controller;
 
 import com.example.compound.use_cases.BudgetManager;
-import com.example.compound.use_cases.ExpenseManager;
-//import com.example.compound.use_cases.gateways.RepositoryGateway;
 import com.example.compound.use_cases.CurrentBudgetManager;
-//import com.example.compound.use_cases.gateways.BudgetRepositoryGateway;
-//import com.example.compound.use_cases.gateways.ItemRepositoryGateway;
-//import com.example.compound.use_cases.gateways.GroupRepositoryGateway;
-import com.example.compound.use_cases.gateways.RepositoryGateway;
+import com.example.compound.use_cases.CurrentGroupManager;
+import com.example.compound.use_cases.ExpenseManager;
+import com.example.compound.use_cases.gateways.RepositoryGatewayI;
+import com.example.compound.use_cases.transfer_data.BudgetTransferData;
+import com.example.compound.use_cases.transfer_data.GroupTransferData;
+import com.example.compound.use_cases.transfer_data.ItemTransferData;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
+/**
+ * A Controller managing input and output for Budget use cases.
+ */
+@RestController
+@RequestMapping("/api/budget")
 public class BudgetController {
-    private final Integer GUID;
-    private final CurrentBudgetManager currentBudgetManager;
     private final BudgetManager budgetManager;
+    private final CurrentGroupManager currentGroupManager;
+    private final CurrentBudgetManager currentBudgetManager;
     private final ExpenseManager expenseManager;
-    private final String[] selectionActions = {
-            "Select a budget",
-            "Create a new budget",
-            "Exit"
-    };
-    private final String[] budgetActions = {
-            "Add an item",
-            "Change the quantity of an item",
-            "Remove an item",
-            "Change the spending limit",
-            "Convert the items in this budget into expenses and add them to the group",
-            "Delete this budget", // TODO: Eliminate percentages method in Budget and interactor if eliminating categories?
-            "Exit"
-    };
 
-    public BudgetController(Integer GUID,
-//                            BudgetRepositoryGateway budgetRepositoryGateway,
-//                            GroupRepositoryGateway groupRepositoryGateway,
-//                            ItemRepositoryGateway itemRepositoryGateway,
-                            RepositoryGateway repositoryGateway,
-                            ExpenseManager expenseManager) {
-        this.GUID = GUID;
-//        this.currentBudgetManager = new CurrentBudgetManager(budgetRepositoryGateway);
-        this.currentBudgetManager = new CurrentBudgetManager(repositoryGateway);
-//        this.budgetManager = new BudgetManager(budgetRepositoryGateway, groupRepositoryGateway, itemRepositoryGateway);
-        this.budgetManager = new BudgetManager(repositoryGateway);
-        this.expenseManager = expenseManager;
+    /**
+     * Construct a new BudgetController with the given parameters.
+     * @param currentGroupManager a use case containing the current group
+     * @param budgetRepository    the repository for budgets
+     * @param itemRepository      the repository for items
+     * @param groupRepository     the repository for groups
+     */
+    public BudgetController(CurrentGroupManager currentGroupManager,
+                            RepositoryGatewayI<BudgetTransferData> budgetRepository,
+                            RepositoryGatewayI<ItemTransferData> itemRepository,
+                            RepositoryGatewayI<GroupTransferData> groupRepository) {
+        this.currentBudgetManager = new CurrentBudgetManager(budgetRepository);
+        this.budgetManager = new BudgetManager(budgetRepository, groupRepository, itemRepository);
+        this.expenseManager = new ExpenseManager(null); // TODO
+        this.currentGroupManager = currentGroupManager;
+//        this.expenseManager = new ExpenseManager(expenseRepository);
     }
 
     /**
-     * Request that the user take an action relating to managing a list of budgets associated with a Group and then take
-     * the appropriate action.
-     * @param inOut the user interface object
+     * Create a new budget with the given name and spending limit.
+     * @param name     the budget's name
+     * @param maxSpend the budget's spending limit
+     * @return whether the budget was created
      */
-    public void groupBudgetsDashboard(InOut inOut) {
-        boolean isInBudgetSelection = true;
-        while (isInBudgetSelection) { // TODO: Variable needed? or just while (true)?
-            int input = inOut.getActionView(selectionActions);
-
-            switch (input) {
-                case 1 -> {
-                    // Output list of Budgets
-                    inOut.sendOutput("The budgets in this group:");
-                    List<String> budgets = budgetManager.getBudgetNameList(GUID);
-
-                    // Get budget choice
-                    if (budgets.size() == 0) {
-                        inOut.sendOutput("This group does not have any budgets yet.");
-                    } else {
-                        int budgetInput = inOut.getActionView(budgets.toArray(new String[0])); // TODO: Currently, prints to choose an action; change to choose a budget; change to getChoiceView?
-                        String budgetName = budgets.get(budgetInput - 1);
-                        String BUID = budgetManager.getBUIDFromName(budgetName);
-                        currentBudgetManager.setCurrentBudget(BUID);
-                        budgetDashboard(inOut, currentBudgetManager);
-                    }
-                }
-                case 2 -> {
-                    String name = inOut.requestInput("the name of the budget");
-                    double maxSpend = requestDouble(inOut, "the maximum amount of money that can be spent on " +
-                            "items in this budget.\nDo not include a dollar sign. For example: 12.34");
-
-                    if (budgetManager.create(GUID, name, maxSpend)) {
-                        inOut.sendOutput("A new budget was successfully added to the given group.");
-                    } else {
-                        inOut.sendOutput("The budget could not be added to the given group. Please try again.");
-                    }
-                }
-                case 3 -> isInBudgetSelection = false;
-            }
-        }
+    @PutMapping("/create")
+    public boolean createBudget(@RequestParam String name, @RequestParam double maxSpend) {
+        return budgetManager.create(currentGroupManager.getCurrentGroupUID(), name, maxSpend);
     }
 
     /**
-     * Request that the user enter a double for the given attribute via the user interface and return the input double.
-     * @param inOut the user interface object
-     * @param request the attribute for which the user is requested to input a double
-     * @return the double input by the user
+     * Return a list of the names of the budgets in the current budget repository.
+     * @return a list of the names of the budgets in the current budget repository
      */
-    public double requestDouble(InOut inOut, String request) {
-        String maxSpendInput = inOut.requestInput(request);
+    @GetMapping("/select")
+    public List<String> getBudgets() {
+        return budgetManager.getBudgetNameList(currentGroupManager.getCurrentGroupUID());
+    }
+
+    /**
+     * Given a valid index for the list returned by getBudgets, set the current budget to the budget with the name
+     * corresponding to the index.
+     * @param position a valid index for the list returned by getBudgets
+     */
+    @PostMapping("/select")
+    public void setBudget(@RequestParam int position) {
+        List<String> names = budgetManager.getBudgetNameList(currentGroupManager.getCurrentGroupUID());
+        String BUID = budgetManager.getBUIDFromName(names.get(position));
+        currentBudgetManager.setCurrentBudget(BUID);
+    }
+
+    /**
+     * Return a list of the names of the items in the current item repository.
+     * @return a list of the names of the items in the current item repository
+     */
+    @GetMapping("/select")
+    public List<String> getItems() {
+        return budgetManager.getBudgetNameList(currentGroupManager.getCurrentGroupUID());
+    }
+
+    /**
+     * Add an item with the given name, cost, and quantity to the current budget.
+     * @param name     the item's name
+     * @param cost     the item's cost
+     * @param quantity the item's quantity
+     * @return whether the item was added
+     */
+    @PutMapping("/item/create")
+    public boolean addItem(@RequestParam String name, @RequestParam double cost, @RequestParam int quantity) {
         try {
-            return Double.parseDouble(maxSpendInput);
-        } catch (NumberFormatException e) {
-            System.out.println("Please enter a valid amount of money.");
-            return requestDouble(inOut, request);
+            Objects.requireNonNull(budgetManager.addItem(currentBudgetManager.getCurrentBudgetUID(), name,
+                    cost, quantity));
+        } catch (NullPointerException e) {
+            return false;
         }
+        return true;
     }
 
     /**
-     * Request that the user enter an integer for the given attribute via the user interface and return the input
-     * integer.
-     * @param inOut the user interface object
-     * @param request the attribute for which the user is requested to input an integer
-     * @return the integer input by the user
+     * Given a valid index for the list returned by getItems, change the quantity of the item with the name
+     * corresponding to the index to the given value
+     * @param position    a valid index for the list returned by getItems
+     * @param newQuantity the item's new quantity
+     * @return whether the quantity was changed
      */
-    public int requestInt(InOut inOut, String request) {
-        String maxSpendInput = inOut.requestInput(request);
-        try {
-            return Integer.parseInt(maxSpendInput);
-        } catch (NumberFormatException e) {
-            System.out.println("Please enter a valid amount of money.");
-            return requestInt(inOut, request);
-        }
+    @PostMapping("/item/update")
+    public boolean changeItemQuantity(@RequestParam int position, @RequestParam int newQuantity) {
+        String IUID = getIUIDFromPosition(position);
+        return budgetManager.changeItemQuantity(IUID, newQuantity);
     }
 
     /**
-     * Request that the user take an action relating to managing a single budget and then take the appropriate action.
-     * @param inOut the user interface object
-     * @param currentBudgetManager the CurrentBudgetManager instance storing the current budget
+     * Given a valid index for the list returned by getItems, remove the item with the name corresponding to the index.
+     * @param position a valid index for the list returned by getItems
+     * @return whether the item was removed
      */
-    public void budgetDashboard(InOut inOut, CurrentBudgetManager currentBudgetManager) {
-        while (true) {
-            // Return an integer between 1 and the number of actions, inclusive
-            int input = inOut.getActionView(budgetActions);
-
-            switch (input) {
-                case 1 -> {
-                    String name = inOut.requestInput("the item's name");
-                    double cost = requestDouble(inOut, "the item's cost");
-                    int quantity = requestInt(inOut, "the item's quantity");
-                    budgetManager.addItem(currentBudgetManager.getCurrentBudgetUID(), name, cost, quantity);
-                    inOut.sendOutput("The item was successfully created.");
-                }
-                case 2 -> {
-                    String IUID = getIUID(inOut, currentBudgetManager);
-                    int newQuantity = requestInt(inOut, "the new quantity");
-                    budgetManager.changeItemQuantity(IUID, newQuantity);
-                }
-                case 3 -> {
-                    String IUID = getIUID(inOut, currentBudgetManager);
-                    budgetManager.removeItem(IUID);
-                }
-                case 4 -> {
-                    double newMaxSpend = requestDouble(inOut, "the new spending limit");
-                    budgetManager.setMaxSpend(currentBudgetManager.getCurrentBudgetUID(), newMaxSpend);
-                }
-                case 5 -> budgetManager.addExpensesToGroup(GUID, currentBudgetManager.getCurrentBudgetUID(),
-                        expenseManager);
-                case 6 -> {
-//                    if (budgetManager.remove(GUID, currentBudgetManager.getCurrentBudgetUID())) {
-                    if (budgetManager.remove(GUID, 1)) {
-                        inOut.sendOutput("The item was removed.");
-                    } else {
-                        inOut.sendOutput("The item could not be removed. Please try again.");
-                    }
-                }
-                case 7 -> {
-                    return;
-                }
-            }
-        }
+    @DeleteMapping("/item/update")
+    public boolean removeItem(@RequestParam int position) {
+        String IUID = getIUIDFromPosition(position);
+        return budgetManager.removeItem(IUID);
     }
 
     /**
-     * Output a list of items in the current budget and return the IUID of the item chosen by the user.
-     * @param inOut the user interface object
-     * @param currentBudgetManager the CurrentBudgetManager instance storing the current budget
-     * @return the IUID of the item chosen by the user
+     * Change the spending limit of the current budget to the given value.
+     * @param newMaxSpend the new spending limit
+     * @return whether the spending limit was changed
      */
-    public String getIUID(InOut inOut, CurrentBudgetManager currentBudgetManager) {
-        List<String> items = budgetManager.getItems(currentBudgetManager.getCurrentBudgetUID());
-        int itemInput = inOut.getActionView(items.toArray(new String[0])); // TODO: Currently, prints to choose an action; change to choose a budget; change to getChoiceView?
-        String itemName = items.get(itemInput - 1);
-        return budgetManager.getIUIDFromName(itemName);
+    @PostMapping("/update")
+    public boolean changeSpendingLimit(@RequestParam double newMaxSpend) {
+        return budgetManager.setMaxSpend(currentBudgetManager.getCurrentBudgetUID(), newMaxSpend);
+    }
+
+    /**
+     * Convert the items in the current budget into expenses and add them to the current group.
+     * @return whether the expenses were added
+     */
+    @PostMapping("/update")
+    public boolean convertToExpenses() {
+        return budgetManager.addExpensesToGroup(currentGroupManager.getCurrentGroupUID(),
+                currentBudgetManager.getCurrentBudgetUID(), expenseManager);
+    }
+
+    /**
+     * Delete the current budget.
+     * @return whether the budget was deleted
+     */
+    @DeleteMapping("/update")
+    public boolean deleteBudget() {
+        return budgetManager.remove(currentGroupManager.getCurrentGroupUID(), currentBudgetManager.getCurrentBudgetUID());
+    }
+
+    /**
+     * Given a valid index for the list returned by getItems, return the name corresponding to the index.
+     * @param position a valid index for the list returned by getItems
+     * @return the name corresponding to the given index
+     */
+    private String getIUIDFromPosition(int position) {
+        List<String> names = budgetManager.getItems(currentBudgetManager.getCurrentBudgetUID());
+        return budgetManager.getIUIDFromName(names.get(position));
     }
 }
